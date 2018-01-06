aac418bfdfa8137286ed1716474bc756efa3346e
Ally <allyrvargas@gmail.com>
2018-01-05 16:08:22 -0800

    As a user, I want to retrieve a list of my messages, respond to an existing message, and create a new message thread

--- lib/kele.rb
+++ lib/kele.rb
@@ -21,4 +21,24 @@ class Kele
       response = self.class.get("https://www.bloc.io/api/v1/mentors/#{mentor_id}/student_availability", headers: { "authorization" => @auth_token })  
       @mentor_availability = JSON.parse(response.body)
     end
+
+    def get_messages(page_num=nil)
+      if page_num == nil
+        response = self.class.get("https://www.bloc.io/api/v1/message_threads"), headers: { "authorization" => @auth_token })
+      else
+        response = self.class.get(api_url("https://www.bloc.io/api/v1/message_threads?page=#{page_num}"), headers: { "authorization" => @auth_token })
+      end
+      @messages = JSON.parse(response.body)
+    end
+
+    def create_message(sender, recipient_id, subject, stripped_text)
+      response = self.class.post("https://www.bloc.io/api/v1/messages"), 
+        body: {
+          "sender": sender,
+          "recipient_id": recipient_id,
+          "subject": subject,
+          "stripped_text": stripped_text
+      },
+        headers: {"authorization" => @auth_token})
+    end
 end
